b7cc2f0141906745019fff40bc6e1e6b844c11de
Harley Alexander <43975092+mayteio@users.noreply.github.com>
2023-07-10 01:26:13 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,15 @@ import { Box } from "../components/layout";
 import { Button } from "../components/button";
 import { Heading } from "../components/typography/heading";
 import { TextField } from "../components/text-field";
-import { Avatar as ArkAvatar, AvatarFallback } from "@ark-ui/react/avatar";
-
-export function Avatar() {
-  return (
-    <ArkAvatar>
-      <AvatarFallback>SU</AvatarFallback>
-    </ArkAvatar>
-  );
-}
+import { Avatar, AvatarFallback } from "@ark-ui/react/avatar";
 
 export default function Index() {
   return (
     <Box m={4} p={6} border="1px solid" borderColor="light" borderRadius="md">
       <Heading mb="24px">Hello panda 🐼!</Heading>
-      <Avatar />
+      <Avatar>
+        <AvatarFallback>SU</AvatarFallback>
+      </Avatar>
       <TextField
         mb="24px"
         label="Example text field"
